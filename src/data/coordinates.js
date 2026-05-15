f276619b8345d51022866dc43629c567b10178dc
Charity Sandra Hombarume <charityshombarume03@gmail.com>
2026-05-14 18:27:13 -0800

    Added coordinate.js

---
+++ src/data/coordinates.js
@@ -0,0 +1,27 @@
+export const coords = {
+  "Main Library": [-17.7848, 31.0525],
+  "Admin Block": [-17.7845, 31.0530],
+  "Bus Terminus": [-17.7865, 31.0548],
+  "NC1": [-17.7855, 31.0538]
+  
+  "Student Union": [-17.7850, 31.0518],
+  "Faculty of Science": [-17.7838, 31.0522],
+  "Faculty of Engineering": [-17.7835, 31.0535],
+  "Faculty of Law": [-17.7852, 31.0510],
+
+  "Faculty of Arts (CM)": [-17.7843, 31.0515],
+  "Faculty of Education": [-17.7858, 31.0528],
+  "Faculty of Arts (AR)": [-17.7841, 31.0508],
+
+  "Faculty of Vet Science": [-17.7860, 31.0535],
+  "Faculty of Agriculture": [-17.7862, 31.0520],
+  "Faculty of Social Sciences": [-17.7847, 31.0505],
+
+  "Beit Hall": [-17.7830, 31.0528],
+  "Swinton Hall": [-17.7833, 31.0515],
+  "Manfred Hodson Hall": [-17.7828, 31.0540],
+
+  "Sports Pavilion": [-17.7820, 31.0545],
+  "Health Centre": [-17.7855, 31.0515],
+  "Computer Centre": [-17.7842, 31.0533]
+};
